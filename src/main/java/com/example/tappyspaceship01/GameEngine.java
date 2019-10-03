f978c1df1d48a333061ad226703fe34db6709bcb
AJAY-MAC <letv4545@gmail.com>
2019-10-03 12:12:30 -0400

    Added Lanes and Player , lives and score sprite, created player object

--- src/main/java/com/example/tappyspaceship01/GameEngine.java
+++ src/main/java/com/example/tappyspaceship01/GameEngine.java
@@ -31,10 +31,13 @@ public class GameEngine extends SurfaceView implements Runnable {
     Thread gameThread;
 
 
+
     // drawing variables
     SurfaceHolder holder;
     Canvas canvas;
     Paint paintbrush;
+    Player player;
+    boolean playerMoveUp;
 
 
 
@@ -51,6 +54,14 @@ public class GameEngine extends SurfaceView implements Runnable {
     // ----------------------------
     // ## GAME STATS
     // ----------------------------
+    int score = 0;
+    int lives = 3;
+    int laneRightX;
+    int lane1Y;
+    int lane2Y;
+    int lane3Y;
+    int lane4Y;
+    int laneDistanceDifference = 200;
 
 
     public GameEngine(Context context, int w, int h) {
@@ -61,6 +72,13 @@ public class GameEngine extends SurfaceView implements Runnable {
 
         this.screenWidth = w;
         this.screenHeight = h;
+        //this.laneRightX = this.screenWidth -200;
+//        this.lane1Y = this.screenHeight/6;
+//        this.lane2Y = this.screenHeight/6+200;
+//        this.lane3Y = this.screenHeight/6+400;
+//        this.lane4Y = this.screenHeight/6+500;
+
+        player =  new Player(context,this.screenWidth - 200,(this.screenHeight/6+200) - 150);
 
         this.printScreenInfo();
     }
@@ -127,7 +145,16 @@ public class GameEngine extends SurfaceView implements Runnable {
 
             // configure the drawing tools
             this.canvas.drawColor(Color.argb(255,255,255,255));
-            paintbrush.setColor(Color.WHITE);
+            paintbrush.setStyle(Paint.Style.STROKE);
+            paintbrush.setColor(Color.BLACK);
+            paintbrush.setTextSize(60);
+            this.canvas.drawRect(20,this.screenHeight/6,this.screenWidth-300,this.screenHeight/6 + 20,paintbrush);
+            this.canvas.drawRect(20,this.screenHeight/6+200,this.screenWidth-300,this.screenHeight/6+200 + 20,paintbrush);
+            this.canvas.drawRect(20,this.screenHeight/6+400,this.screenWidth-300,this.screenHeight/6+400 + 20,paintbrush);
+            this.canvas.drawRect(20,this.screenHeight/6+600,this.screenWidth-300,this.screenHeight/6+600 + 20,paintbrush);
+            this.canvas.drawText("SCORE = " +this.score+"",this.screenWidth-300,50,paintbrush);
+            this.canvas.drawText("LIVES = " +this.lives+"",this.screenWidth-700,50,paintbrush);
+            this.canvas.drawBitmap(this.player.getImage(),this.player.getxPosition(),this.player.getyPosition(),paintbrush);
 
 
             // DRAW THE PLAYER HITBOX
